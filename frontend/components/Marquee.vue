56fcfbd33dabf5795a6807060569a18a8167a6b6
Jack MB <jackmb@Jacks-iMac.local>
2022-05-27 18:03:33 +0100

    how much co2....

--- frontend/components/Marquee.vue
+++ frontend/components/Marquee.vue
@@ -2,7 +2,7 @@
   <div class="wobble">
     <client-only>
       <marquee-text id="marquee" class="pt-6 pb-10 -mt-2" @mouseover="marqueePaused = true" @mouseleave="marqueePaused = false" :repeat="3" :duration="25" :paused="marqueePaused">
-        <span class="text-5xl marquee-text PicNic">The gut goes all the way from your mouth to your bottom !&nbsp;</span>
+        <span class="text-5xl marquee-text PicNic">The gut goes all the way from your mouth to your bottom É·E 0.18g of CO2 was used to load this website É·E&nbsp;</span>
       </marquee-text>
     </client-only>
   </div>
