5ff268779f1f33bbf3ea5a6ce4a73b8ed09f3609
Jack MB <jackmb@Jacks-iMac.local>
2022-05-27 18:13:58 +0100

    speed of marquee better now

--- frontend/components/Marquee.vue
+++ frontend/components/Marquee.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="wobble">
     <client-only>
-      <marquee-text id="marquee" class="pt-6 pb-10 -mt-2" @mouseover="marqueePaused = true" @mouseleave="marqueePaused = false" :repeat="3" :duration="25" :paused="marqueePaused">
+      <marquee-text id="marquee" class="pt-6 pb-10 -mt-2" @mouseover="marqueePaused = true" @mouseleave="marqueePaused = false" :repeat="3" :duration="35" :paused="marqueePaused">
         <span class="text-5xl marquee-text PicNic">The gut goes all the way from your mouth to your bottom ... 0.18g of CO2 was used to load this website ...&nbsp;</span>
       </marquee-text>
     </client-only>
