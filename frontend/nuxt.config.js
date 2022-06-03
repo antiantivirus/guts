635dc6d1f8a1c5083ceda5060f7499dcbe023f0c
Jack MB <jackmb@Jacks-iMac.local>
2022-06-03 17:32:02 +0100

    social social

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -15,7 +15,7 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'A club night with a difference. GUTS lets you prod and poke the internals to see how it works - from budgets, emails and assets to interviews with others working in electronic music.' },
-      { hid: 'og:image', property: 'og:image', content: '/social-image.jpg' }
+      { hid: 'og:image', property: 'og:image', content: '/social.jpg' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
