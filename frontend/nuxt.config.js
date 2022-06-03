ac1d4e7f9e3a7fdd6ac528ab10a72f4affde068a
Jack MB <jackmb@Jacks-iMac.local>
2022-06-03 17:28:20 +0100

    added social image

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -14,7 +14,8 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'A club night with a difference. GUTS lets you prod and poke the internals to see how it works - from budgets, emails and assets to interviews with others working in electronic music.' }
+      { hid: 'description', name: 'description', content: 'A club night with a difference. GUTS lets you prod and poke the internals to see how it works - from budgets, emails and assets to interviews with others working in electronic music.' },
+      { hid: 'og:image', property: 'og:image', content: '/social-image.jpg' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
