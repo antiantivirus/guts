3c23ed3ffae04ecf814ce7752f41944b0b8e84ba
Jack MB <jackmb@Jacks-iMac.local>
2022-06-11 09:44:07 +0100

    fixed some responsive issues with plaster

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="container">
-    <div id="home-grid" class="grid lg:grid-cols-2 lg:content-center">
-      <img class="drop-shadow-png lg:row-span-2 floating" src="~assets/img/gutsinside.png" />
-      <div class="lg:order-last order-first">
-        <h1 class="title order-first lg:order-2 xl:px-8">
+    <div id="home-grid" class="grid xl:grid-cols-2 xl:content-center">
+      <img class="drop-shadow-png xl:row-span-2 floating" src="~assets/img/gutsinside.png" />
+      <div class="xl:order-last order-first">
+        <h1 class="title order-first xl:px-8">
           <img class="drop-shadow-png" alt="guts" src="~assets/img/guts-logo-2.png"/>
         </h1>
-        <h2 class="lg:px-8 text-center mx-auto">A club night with a difference. GUTS lets you prod and poke the internals to see how it works - from budgets, emails and assets to interviews with others working in electronic music.
+        <h2 class="xl:px-8 text-center mx-auto">A club night with a difference. GUTS lets you prod and poke the internals to see how it works - from budgets, emails and assets to interviews with others working in electronic music.
         <br/>
         <br/>
         Gut exploration and club night coming very soon… 
         </h2>
-        <a class="mx-2 lg:mx-auto text-center block PicNic text-3xl lg:text-5xl mt-6 lg:mt-12" href="https://ra.co/events/1546931" target="_blank">Book your tickets</a>
+        <a class="mx-2 mx-auto text-center block PicNic text-3xl md:text-5xl mt-6 lg:mt-12" href="https://ra.co/events/1546931" target="_blank">Book your tickets</a>
       </div>
     </div>
     <!-- <h2 class="max-w-sm text-center mx-auto lg:hidden">
